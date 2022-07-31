abe538d2fc2f69a6689ec4b218b75dd5a326c14e
prathamesh lade <prathameshlade1@gmail.com>
2022-07-31 21:38:40 +0530

    first commit

---
+++ src/utils.js
@@ -0,0 +1,69 @@
+import { v4 as uuidv4 } from "uuid";
+function chillHop() {
+  return [
+    {
+      name: "Embers",
+      artist: "MMolly Mcphaul,Enluv",
+      cover: "https://i.scdn.co/image/ab67616d0000b2738ba0f5ac658406e23b219efb",
+      id: uuidv4(),
+      active: true,
+      color: ["red", "black"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=30135"
+    },
+    {
+      name: "Rewind",
+      artist: "SwuM,afternoon bike ride",
+      cover: "https://i.scdn.co/image/ab67616d0000b2732eeba3427fdcb46f5871d123",
+      id: uuidv4(),
+      active: false,
+      color: ["purple", "black"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=30134"
+    },
+    {
+      name: "Drift",
+      artist: "Somni",
+      cover: "https://i.scdn.co/image/ab67616d0000b2738dfad56234886eb65b054e80",
+      id: uuidv4(),
+      active: false,
+      color: ["purple", "black"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=30133"
+    },
+    {
+      name: "Lost",
+      artist: "Philanthrope ,Yasper",
+      cover: "https://i.scdn.co/image/ab67616d0000b2738ba0f5ac658406e23b219efb",
+      id: uuidv4(),
+      active: false,
+      color: ["purple", "black"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=30132"
+    },
+    {
+      name: "Leaf contour",
+      artist: "Arijit Singh ft. KSW",
+      cover: "https://i.scdn.co/image/ab67616d0000b2739f22293f14433a0549076698",
+      id: uuidv4(),
+      active: false,
+      color: ["purple", "black"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=30130"
+    },
+    {
+      name: "Better",
+      artist: "afternoon bike ride",
+      cover: "https://i.scdn.co/image/ab67616d0000b2738ba0f5ac658406e23b219efb",
+      id: uuidv4(),
+      active: false,
+      color: ["purple", "black"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=30128"
+    },
+    {
+      name: "Foliage bokeh",
+      artist: "santpoort",
+      cover: "https://i.scdn.co/image/ab67616d0000b273ea0174741f00af69646edc8e",
+      id: uuidv4(),
+      active: false,
+      color: ["purple", "black"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=30127"
+    }
+  ];
+}
+export default chillHop;
